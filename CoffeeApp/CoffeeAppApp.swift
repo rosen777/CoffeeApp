57851a84fd84787c6f9e9fd71a557bd26ab491af
Rosen Toshev <rtoscheff@gmail.com>
2025-03-11 23:19:06 +0000

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Rosen Toshev on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeAppTests/CoffeeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppTests.swift
+//  CoffeeAppTests
+//
+//  Created by Rosen Toshev on 11/03/2025.
+//
+
+import Testing
+@testable import CoffeeApp
+
+struct CoffeeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
